c9c0d0612e3ab70626ac384e4188e110f53aba47
Daniel Martinez <dmartz86@gmail.com>
2016-03-09 11:15:27 -0500

    Update assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,13 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( support/phantomjs-shims.self.js )
+Rails.application.config.assets.precompile += %w( jquery.self.js )
+Rails.application.config.assets.precompile += %w( jquery_ujs.self.js )
+Rails.application.config.assets.precompile += %w( turbolinks.self.js )
+Rails.application.config.assets.precompile += %w( books.self.js )
+Rails.application.config.assets.precompile += %w( application.self.js )
+Rails.application.config.assets.precompile += %w( spec_helper.self.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
