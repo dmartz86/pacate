b2bfc0a3dd115a6c951b87849bfa29456584432d
Daniel Martinez <dmartz86@gmail.com>
2016-03-10 08:45:25 -0500

    Add React

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-#run coverage
-Run teaspoon
+# rails seed
+This is a preconfigured project.
+
+## tools
+
+## coverage
+Add your JavaScript files to
+````sh
+config/initializers/assets.rb
+````
+
+Execute teaspoon
 ````sh
 bundle exec teaspoon --coverage=default
 ````
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,15 +3,24 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 javascript_files = %w{
- support/phantomjs-shims.self.js
- jquery.self.js
- jquery_ujs.self.js
- turbolinks.self.js
- books.self.js
- application.self.js
- spec_helper.self.js
- insta.self.js
- library.self.js
+  support/phantomjs-shims.self.js
+  jquery.self.js
+  jquery_ujs.self.js
+  turbolinks.self.js
+  books.self.js
+  application.self.js
+  spec_helper.self.js
+  insta.self.js
+  library.self.js
+  react.self.js
+  react_ujs_mount.self.js
+  react_ujs_turbolinks.self.js
+  react_ujs_turbolinks_classic.self.js
+  react_ujs_turbolinks_classic_deprecated.self.js
+  react_ujs_native.self.js
+  react_ujs_event_setup.self.js
+  react_ujs.self.js
+  components.self.js
 }
 Rails.application.config.assets.precompile += javascript_files
 
